0baf70954d81a4cff3d409cb5e355f87822f9187
emmansb <biodunadekiitan@gmail.com~>
2022-04-27 17:41:43 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+function iconFaq() {
+    document.querySelector("p.wow").textContent = "ZEDD COIN(ZCN) is Zeddpays currency , your deposits are converted to zeddcoin and you make purchases with it too ❤️"
+}
